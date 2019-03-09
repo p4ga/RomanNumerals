3ac41f356b2b79a4f1541a6d4eb0028c49c31225
Andrew <letzerich@gmail.com>
2019-03-09 10:50:40 -0600

    converted numbers 1 and 2 from arabic to roman numerals

---
+++ src/main/java/com/pairinggood/RomanNumeral.java
@@ -0,0 +1,12 @@
+package com.pairinggood;
+
+public class RomanNumeral {
+
+    public String convert(int arabicNumber) {
+        if (arabicNumber == 1) {
+            return "I";
+        } else {
+            return "II";
+        }
+    }
+}
--- src/test/java/com/pairinggood/RomanNumeralsTest.java
+++ src/test/java/com/pairinggood/RomanNumeralsTest.java
@@ -1,4 +1,36 @@
 package com.pairinggood;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RomanNumeralsTest {
+
+    @Test
+    public void whenArabicNumberOneEntered_RomanNumeral_I_Returned(){
+
+        //arrange
+        RomanNumeral converter = new RomanNumeral();
+
+        //act
+        String romanNumeral = converter.convert(1);
+
+        //assert
+        Assert.assertEquals("I",romanNumeral);
+
+    }
+
+    @Test
+    public void whenArabicNumberTwoEntered_RomanNumeral_II_Returned(){
+
+        //arrange
+        RomanNumeral converter = new RomanNumeral();
+
+        //act
+        String romanNumeral = converter.convert(2);
+
+        //assert
+        Assert.assertEquals("II",romanNumeral);
+
+    }
+
 }
