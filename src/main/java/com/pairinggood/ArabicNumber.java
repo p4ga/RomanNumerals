adc82e6d50495c32c654eb0444bc500aed7f3a9c
Tyler Ward <tylerjward@gmail.com>
2019-03-14 00:26:21 -0500

    allowed for the passing of I, II, and III to convert to 1,2,3 respectively

--- src/main/java/com/pairinggood/ArabicNumber.java
+++ src/main/java/com/pairinggood/ArabicNumber.java
@@ -4,6 +4,18 @@ public class ArabicNumber {
 
 
     public int convert(String romanNumeral) {
-        return 1;
+
+        final char[] letters = romanNumeral.toCharArray();
+
+        int arabicNumber = 0;
+
+        for (char letter : letters) {
+           if (letter == 'I') {
+               arabicNumber = arabicNumber + 1;
+
+           }
+
+        }
+        return arabicNumber;
     }
 }
--- src/test/java/com/pairinggood/ArabicNumberTest.java
+++ src/test/java/com/pairinggood/ArabicNumberTest.java
@@ -9,13 +9,41 @@ public class ArabicNumberTest {
     public void whenRomanNumeral_I_EnteredArabicNumber_1_Returned(){
 
         //arrange
+        ArabicNumber converterZ = new ArabicNumber();
+
+        //act
+        int arabicNumberZ = converterZ.convert("I");
+
+        //assert
+        Assert.assertEquals(1, arabicNumberZ);
+
+    }
+
+    @Test
+    public void whenRomanNumeral_II_EnteredArabicNumber_2_Returned(){
+
+        //arrange
         ArabicNumber converter = new ArabicNumber();
 
         //act
-        int arabicNumber = converter.convert("I");
+        int arabicNumber = converter.convert("II");
 
         //assert
-        Assert.assertEquals(1, arabicNumber);
+        Assert.assertEquals(2, arabicNumber);
+
+    }
+
+    @Test
+    public void whenRomanNumeral_III_EnteredArabicNumber_3_Returned(){
+
+        //arrange
+        ArabicNumber converter = new ArabicNumber();
+
+        //act
+        int arabicNumber = converter.convert("III");
+
+        //assert
+        Assert.assertEquals(3, arabicNumber);
 
     }
 
